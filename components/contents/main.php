d1bb45f347ce3770de4e677624c2003dc4dd076a
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-01-30 14:24:47 +0300

    Change category viewing mechanism.

--- components/contents/main.php
+++ components/contents/main.php
@@ -1,19 +1,5 @@
         <div class="container clearfix">
             <main class="content">
                 <!-- for-example -->
-                <p style="text-align: center; font-size: 30px; margin: 0; padding: 1.5em 0;">Выберите категорию:</p>
-                <ul>
-                    <?php
-                    $str_out_cat = "SELECT * FROM `category`";
-                    $run_out_cat = mysqli_query($connect, $str_out_cat);
-
-                    while ($out_cat = mysqli_fetch_array($run_out_cat)) {
-                        echo "
-                            <li><a href=category.php?id_cat=$out_cat[id] style='text-align: center; font-size: 30px;'>
-                                $out_cat[category_name]
-                            </a></li>
-                        ";
-                    }
-                    ?>
-                </ul>
+                <p style="text-align: center; font-size: 30px; margin: 0; padding: 1.5em 0;">Выберите категорию в боковой панели!</p>
             </main>
--- components/sidebar.php
+++ components/sidebar.php
@@ -1,4 +1,24 @@
             <aside class="sidebar sidebar1">
                 <p style="text-align: center;"><a href="/">Главная</a></p>
+                <div class="nav">
+                    <ul class="topmenu">
+                        <li><a href="#">Категории</a>
+                            <ul class="submenu">
+                                <?php
+                                $str_out_cat = "SELECT * FROM `category`";
+                                $run_out_cat = mysqli_query($connect, $str_out_cat);
+
+                                while ($out_cat = mysqli_fetch_array($run_out_cat)) {
+                                    echo "
+                                        <li><a href=category.php?id_cat=$out_cat[id] style='text-align: center; font-size: 30px;'>
+                                            $out_cat[category_name]
+                                        </a></li>
+                                    ";
+                                }
+                                ?>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </aside>
         </div>
